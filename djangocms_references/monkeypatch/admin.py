48da85e8b63edf1292fe40005eebcc36a86b9e94
Evan-Pi <vaggelispissinas@gmail.com>
2024-01-26 13:32:52 +0100

    Include get_list_actions back to AliasContentAdmin

--- djangocms_references/monkeypatch/admin.py
+++ djangocms_references/monkeypatch/admin.py
@@ -3,36 +3,31 @@ from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 
 from djangocms_alias import admin
+from djangocms_alias.models import AliasContent
 
+class AliasContentAdminExtended(admin.AliasContentAdmin):
+    def _get_references_link(self, obj, request):
+        alias_content_type = ContentType.objects.get(
+            app_label=obj.alias._meta.app_label,
+            model=obj.alias._meta.model_name,
+        )
 
-def _get_references_link(self, obj, request):
-    alias_content_type = ContentType.objects.get(
-        app_label=obj.alias._meta.app_label,
-        model=obj.alias._meta.model_name,
-    )
+        url = reverse_lazy(
+            "djangocms_references:references-index",
+            kwargs={"content_type_id": alias_content_type.id, "object_id": obj.alias.id}
+        )
 
-    url = reverse_lazy(
-        "djangocms_references:references-index",
-        kwargs={"content_type_id": alias_content_type.id, "object_id": obj.alias.id}
-    )
+        return render_to_string("djangocms_references/references_icon.html", {"url": url})
 
-    return render_to_string("djangocms_references/references_icon.html", {"url": url})
+    def get_list_actions(self):
+        list_actions = []
 
+        if hasattr(super(), "get_list_actions"):
+            list_actions = super().get_list_actions()
 
-admin.AliasContentAdmin._get_references_link = _get_references_link
-
-
-def get_list_actions(func):
-    """
-    Add references action to alias list display
-    """
-    def inner(self, *args, **kwargs):
-        list_actions = func(self, *args, **kwargs)
         list_actions.append(self._get_references_link)
+
         return list_actions
-    return inner
 
-
-admin.AliasContentAdmin.get_list_actions = get_list_actions(
-    admin.AliasContentAdmin.get_list_actions
-)
+admin.site.unregister(AliasContent)
+admin.site.register(AliasContent, AliasContentAdminExtended)
